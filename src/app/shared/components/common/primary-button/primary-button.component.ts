49c1642c205ffa16dc3efc0d78e843d282e55024
md-nafis-sadik <sadikmdnafis@gmail.com>
2025-04-14 11:26:02 +0600

    menu changes

--- src/app/shared/components/common/primary-button/primary-button.component.ts
+++ src/app/shared/components/common/primary-button/primary-button.component.ts
@@ -8,11 +8,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './primary-button.component.html',
   styleUrl: './primary-button.component.css'
 })
+
 export class PrimaryButtonComponent {
   @Input() text: string = 'Learn more';
   @Input() bgColor: string = '#06ACCE';
   @Input() hoverColor: string = '#0799B7';
   @Input() class: string = '';
+  @Input() fontSize: string = 'text-base';   // optional font size
+  @Input() padding: string = 'py-2';         // optional padding
+  @Input() width: string = 'w-[130px]';      // optional width
+
   hovering = false;
+
+  get combinedClasses(): string {
+    return `
+      mt-4
+      ${this.width}
+      text-white 
+      rounded-lg 
+      transition-colors 
+      cursor-pointer 
+      ${this.fontSize} 
+      ${this.padding} 
+      ${this.class}
+    `;
+  }
 }
+
+
 
