c2bce0f7611c86eeb42084254e220ab780142b56
hridoyhaque01 <hridoyhaque.info@gmail.com>
2025-04-28 13:11:28 +0600

    change: dynamic viewport

---
+++ src/app/services/viewport.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ViewportService {
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  init() {
+    this.setViewportHeight();
+    window.addEventListener('resize', this.setViewportHeight.bind(this));
+  }
+
+  private setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+}
